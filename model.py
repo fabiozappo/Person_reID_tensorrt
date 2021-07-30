loss calculation bringed inside amp context resolved bug!

model.py
@@ -46,12 +46,12 @@ class ClassBlock(nn.Module):
         if droprate > 0:
             add_block += [nn.Dropout(p=droprate)]
         add_block = nn.Sequential(*add_block)
-        add_block.apply(weights_init_kaiming)
+        # add_block.apply(weights_init_kaiming)
 
         classifier = []
         classifier += [nn.Linear(num_bottleneck, class_num)]
         classifier = nn.Sequential(*classifier)
-        classifier.apply(weights_init_classifier)
+        # classifier.apply(weights_init_classifier)
 
         self.add_block = add_block
         self.classifier = classifier
@@ -70,13 +70,13 @@ class ClassBlock(nn.Module):
 # Define the ResNet50-based Model
 class res_net50(nn.Module):
 
-    def __init__(self, class_num, droprate=0.5, circle=False):
+    def __init__(self, class_num, droprate=0.5, circle=False, num_bottleneck=512):
         super(res_net50, self).__init__()
         model_ft = models.resnet50(pretrained=True)
         model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
         self.model = model_ft
         self.circle = circle
-        self.classifier = ClassBlock(2048, class_num, droprate, return_f=circle)
+        self.classifier = ClassBlock(2048, class_num, droprate, return_f=circle, num_bottleneck=num_bottleneck)
 
     def forward(self, x):
         x = self.model.conv1(x)
@@ -96,13 +96,13 @@ class res_net50(nn.Module):
 # Define the ResNet50-based Model
 class res_net18(nn.Module):
 
-    def __init__(self, class_num, droprate=0.5, circle=False):
+    def __init__(self, class_num, droprate=0.5, circle=False, num_bottleneck=512):
         super(res_net18, self).__init__()
         model_ft = models.resnet18(pretrained=True)
         model_ft.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.model = model_ft
         self.circle = circle
-        self.classifier = ClassBlock(512, class_num, droprate, return_f=circle)
+        self.classifier = ClassBlock(512, class_num, droprate, return_f=circle, num_bottleneck=num_bottleneck)
 
     def forward(self, x):
         x = self.model.conv1(x)
@@ -122,14 +122,14 @@ class res_net18(nn.Module):
 # Define the MobilenetV2 based Model
 class mob_net(nn.Module):
 
-    def __init__(self, class_num, droprate=0.5, circle=False):
+    def __init__(self, class_num, droprate=0.5, circle=False, num_bottleneck=512):
         super(mob_net, self).__init__()
         model_ft = models.mobilenet_v2(pretrained=True)
         # avg pooling to global pooling
         model_ft.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.model = model_ft
         self.circle = circle
-        self.classifier = ClassBlock(1280, class_num, droprate, return_f=circle)
+        self.classifier = ClassBlock(1280, class_num, droprate, return_f=circle, num_bottleneck=num_bottleneck)
 
     def forward(self, x):
         x = self.model.features(x)
@@ -142,12 +142,12 @@ class mob_net(nn.Module):
 # Define the squeeze_net-based Model
 class squeeze_net(nn.Module):
 
-    def __init__(self, class_num, droprate=0.5, circle=False):
+    def __init__(self, class_num, droprate=0.5, circle=False, num_bottleneck=512):
         super(squeeze_net, self).__init__()
         model_ft = models.squeezenet1_1(pretrained=True)
         self.model = model_ft
         self.circle = circle
-        self.classifier = ClassBlock(1000, class_num, droprate, return_f=circle)
+        self.classifier = ClassBlock(1000, class_num, droprate, return_f=circle, num_bottleneck=num_bottleneck)
 
     def forward(self, x):
         x = self.model.features(x)
@@ -159,13 +159,13 @@ class squeeze_net(nn.Module):
 
 class deep_net(nn.Module):
 
-    def __init__(self, class_num, droprate=0.5, circle=False):
+    def __init__(self, class_num, droprate=0.5, circle=False, num_bottleneck=512):
         super(deep_net, self).__init__()
         model_ft = Deep(num_classes=class_num)
         model_ft.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.model = model_ft
         self.circle = circle
-        self.classifier = ClassBlock(512, class_num, droprate, return_f=circle)
+        self.classifier = ClassBlock(512, class_num, droprate, return_f=circle, num_bottleneck=num_bottleneck)
 
     def forward(self, x):
         x = self.model.conv(x)
@@ -185,7 +185,7 @@ python model.py
 if __name__ == '__main__':
     # Here I left a simple forward function.
     # Test the model, before you train it.
-    net = mob_net(751)
+    net = mob_net(751, num_bottleneck=128)
     # remove last fc from classifier part
     net.classifier.classifier = nn.Sequential()
     print(net)

train.py
@@ -18,8 +18,8 @@ import os
 from model import res_net50, res_net18, squeeze_net, mob_net, deep_net
 import yaml
 from shutil import copyfile
-from apex.fp16_utils import *
-from apex import amp
+# from apex.fp16_utils import *
+from torch.cuda import amp
 from circle_loss import CircleLoss, convert_label_to_similarity
 
 # TODO: trainare e valutare il modello nelle condizioni del nostro caso d'uso, no interpolazione bicubica, minori dimensioni?
@@ -67,7 +67,7 @@ def save_network(network, epoch_label):
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
     # fp16 training
-    scaler = torch.cuda.amp.GradScaler()
+    scaler = amp.GradScaler()
 
     warm_up = 0.1  # We start from the 0.1*lrRate
     warm_iteration = round(dataset_sizes['train'] / batchsize) * warm_epoch  # first 5 epoch
@@ -100,29 +100,32 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
                 x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
 
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
                 # forward
-                with torch.cuda.amp.autocast():
+                with amp.autocast():
                     if phase == 'val':
                         with torch.no_grad():
                             outputs = model(x)
                     else:
                         outputs = model(x)
 
-                # computing loss
-                if opt.circle:
-                    logits, ff = outputs
-                    fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
-                    ff = ff.div(fnorm.expand_as(ff))
-                    loss = criterion(logits, y) + criterion_circle(*convert_label_to_similarity(ff, y)) / now_batch_size
-                    _, preds = torch.max(logits.data, 1)
-                else:
-                    _, preds = torch.max(outputs.data, 1)
-                    loss = criterion(outputs, y)
+                    # computing loss
+                    if opt.circle:
+                        logits, ff = outputs
+                        fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
+                        ff = ff.div(fnorm.expand_as(ff))
+                        loss = criterion(logits, y) + criterion_circle(*convert_label_to_similarity(ff, y)) / now_batch_size
+                        _, preds = torch.max(logits.data, 1)
+                    else:
+                        _, preds = torch.max(outputs.data, 1)
+                        loss = criterion(outputs, y)
 
-                # warmup
-                if epoch < warm_epoch and phase == 'train':
-                    warm_up = min(1.0, warm_up + 0.9 / warm_iteration)
-                    loss = loss * warm_up
+                    # warmup
+                    if epoch < warm_epoch and phase == 'train':
+                        warm_up = min(1.0, warm_up + 0.9 / warm_iteration)
+                        loss = loss * warm_up
 
                 # backward + optimize
                 if phase == 'train':
@@ -133,8 +136,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                     scaler.step(optimizer)
                     scaler.update()
 
-                    # zero the parameter gradients
-                    optimizer.zero_grad()
 
 
                 # statistics