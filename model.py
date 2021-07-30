74844237bc360e10e260ee33264efd8a78216527
fabio <fabio.zappardino@gmail.com>
2021-07-30 14:54:15 +0200

    deleted usless weight initialization, bias set to false in linear layer before bn

--- model.py
+++ model.py
@@ -6,26 +6,6 @@ from torch.autograd import Variable
 
 from deep import Deep
 
-######################################################################
-def weights_init_kaiming(m):
-    classname = m.__class__.__name__
-    # print(classname)
-    if classname.find('Conv') != -1:
-        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')  # For old pytorch, you may use kaiming_normal.
-    elif classname.find('Linear') != -1:
-        init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
-        init.constant_(m.bias.data, 0.0)
-    elif classname.find('BatchNorm1d') != -1:
-        init.normal_(m.weight.data, 1.0, 0.02)
-        init.constant_(m.bias.data, 0.0)
-
-
-def weights_init_classifier(m):
-    classname = m.__class__.__name__
-    if classname.find('Linear') != -1:
-        init.normal_(m.weight.data, std=0.001)
-        init.constant_(m.bias.data, 0.0)
-
 
 # Defines the new fc layer and classification layer
 # |--Linear--|--bn--|--relu--|--Linear--|
@@ -46,12 +26,10 @@ class ClassBlock(nn.Module):
         if droprate > 0:
             add_block += [nn.Dropout(p=droprate)]
         add_block = nn.Sequential(*add_block)
-        # add_block.apply(weights_init_kaiming)
 
         classifier = []
         classifier += [nn.Linear(num_bottleneck, class_num)]
         classifier = nn.Sequential(*classifier)
-        # classifier.apply(weights_init_classifier)
 
         self.add_block = add_block
         self.classifier = classifier
